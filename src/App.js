842209b690f2606b1fca84c3f82cc7c971515837
theLAZYmd <lazymeep+github@gmail.com>
2020-02-18 13:03:18 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function calcDay() {
+	let date = new Date(Date.now());
+	let day = date.getUTCDay();
+	if (day <= 2) return false;
+	if (day >= 6) return false;
+	return true;
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [isOpen, setIsOpen] = useState(calcDay());
+
+	return (
+		<div className='App'>
+			<div>
+				IS THE UNDIE OPEN TONIGHT?
+			</div>
+			<div>
+				<img
+					src = {'/crest-cardinals-hat.png'}
+					className = "App-logo"
+					alt = "logo"
+				/>
+			</div>
+			<div
+				className='isOpen'
+			>
+				{isOpen ? 'YES' : 'NO'}
+			</div>
+			<br />
+			<div>
+				The undie is open every Wednesday, Thursday, Friday, and Saturday, from 8PM to 11PM.
+			</div>
+		</div>
+	);
 }
 
 export default App;
