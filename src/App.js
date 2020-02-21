0c07efc4e45d97d24c98cf4a85a36a0f5a40cf46
theLAZYmd <lazymeep+github@gmail.com>
2020-02-21 14:04:22 +0000

    Added syncing with google calendar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function calcDay() {
@@ -9,9 +10,57 @@ function calcDay() {
 	return true;
 }
 
+async function getCalendar() {
+	try {
+		let calendarId = '1di06f3pqhr8o0mrefiiouts80@group.calendar.google.com';
+		let today = new Date(Date.now());
+		let res = await axios({
+			baseURL: 'https://clients6.google.com/calendar/v3/calendars/',
+			url: calendarId + '/events',
+			params: {
+				calendarId,
+				singleEvents: true,
+				timeZone: 'Europe/London',
+				maxAttendees: 1,
+				maxResults: 250,
+				sanitizeHtml: true,
+				timeMin: new Date(new Date(today).setUTCDate(today.getUTCDate() - 1)).toISOString(), //'2019-10-27T00:00:00Z',
+				timeMax: new Date(new Date(today).setUTCDate(today.getUTCDate() + 1)).toISOString(), //'2019-12-01T00:00:00Z',
+				key: 'AIzaSyAWgkdBGseBg_AtOB2kfZXWiyp4RM6b45c' //AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs'
+			}
+		});
+		let data = res.data;
+		console.log(data.items);
+		return (data.items || []).map((event) => {
+			return {
+				name: event.summary,
+				day: event.start ? new Date(event.start.date || event.start.dateTime) : new Date(Date.now()),
+				description: event.description || ''
+			}
+		}) || [];
+	} catch (e) {
+		console.error(e);
+	}
+}
+
 export default function App() {
 
 	const [isOpen, setIsOpen] = useState(calcDay());
+	const [events, setEvents] = useState([]);
+
+	useEffect(() => {
+		getCalendar()
+			.then((data) => {
+				console.log(data);
+				for (let event of data) {
+					let status = event.name.trim().toUpperCase();
+					if (status === 'CLOSED') setIsOpen(false);
+					else if (status === 'OPEN') setIsOpen(true);
+				}
+				setEvents(data);
+			});
+	}, [])
+	
 
 	return (
 		<div className='App'>
@@ -30,7 +79,12 @@ export default function App() {
 			>
 				{isOpen ? 'YES' : 'NO'}
 			</div>
-			<br />
+			{events.map((event) => {
+				if (!event.description) return null;
+				return <div className='description' dangerouslySetInnerHTML={{
+					__html: event.description
+				}} />
+			})}
 			<div>
 				The undie is open every Wednesday, Thursday, Friday, and Saturday, from 8PM to 11PM.
 			</div>
