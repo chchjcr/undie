822f18881590e7594fb691c013cc5c9ccd2a0192
theLAZYmd <lazymeep+github@gmail.com>
2020-02-29 18:21:15 +0000

    Updates

---
+++ precache-manifest.2adac0aec2091ad6c6f87d287e66341c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916ed792fdd923ad7650ceea94d6ea98",
+    "url": "/undie/index.html"
+  },
+  {
+    "revision": "6b10e8bceec15ed73902",
+    "url": "/undie/static/css/main.0ee9ebe2.chunk.css"
+  },
+  {
+    "revision": "0d8740293f02639e0379",
+    "url": "/undie/static/js/2.705dbe07.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/undie/static/js/2.705dbe07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b10e8bceec15ed73902",
+    "url": "/undie/static/js/main.6dd9e03c.chunk.js"
+  },
+  {
+    "revision": "86513c7de82c6736c9d9",
+    "url": "/undie/static/js/runtime-main.862e218e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7d07c05227ec68021edf1ae0f06a9cd2.js"
+  "/undie/precache-manifest.2adac0aec2091ad6c6f87d287e66341c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/undie/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
